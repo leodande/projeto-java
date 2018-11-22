91541127f5088fe68c03a0ea3e1bd7e87b8d557e
leodande <leodande@hotmail.com>
2018-11-22 20:35:51 -0300

    infonote_01

---
+++ infonote_01/src/controller/Teste.java
@@ -0,0 +1,21 @@
+package controller;
+
+import model.*;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ItemDePedido Idp1 = new ItemDePedido();
+		Idp1.setQtde(2);
+		Idp1.setSubtotal(22.98);
+		System.out.println(Idp1.toString());
+
+		ItemDePedido Idp2 = new ItemDePedido();
+		Idp2.setQtde(1);
+		Idp2.setSubtotal(98.75);
+		System.out.println(Idp2.toString());
+	}
+
+}
